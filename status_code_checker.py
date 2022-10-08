c3e0ae41ce45bf5cd8d06fdedd9bb7764871d61c
davidjohnca <112810154+davidjohnca@users.noreply.github.com>
2022-10-09 00:47:41 +0100

    Update status_code_checker.py

--- status_code_checker.py
+++ status_code_checker.py
@@ -1,38 +1,30 @@
-# status code checker
 import requests
 import csv
 import time
 
-SLEEP = 0 # Time in seconds the script should wait between requests
+SLEEP = 0
 url_list = []
 url_statuscodes = []
-url_statuscodes.append(["url","status_code"]) # set the file header for output
+url_statuscodes.append(["url","status_code"])
 
 def getStatuscode(url):
     try:
-        r = requests.head(url,verify=False,timeout=5) # it is faster to only request the header
+        r = requests.head(url,verify=False,timeout=5)
         return (r.status_code)
-
     except:
         return -1
 
-# Url checks from file Input
-# use one url per line that should be checked
-with open('list_of_urls_to_check.csv', newline='', enconding='utf8') as f:
+with open('urls.csv', newline='') as f:
     reader = csv.reader(f)
     for row in reader:
         url_list.append(row[0])
-        
-# Loop over full list
+
 for url in url_list:
-    #print(url)
+    print(url)
     check = [url,getStatuscode(url)]
     time.sleep(SLEEP)
     url_statuscodes.append(check)
 
-# Save file
 with open("urls_withStatusCode.csv", "w", newline="") as f:
     writer = csv.writer(f)
     writer.writerows(url_statuscodes)
-
-print('Task complete!')    
