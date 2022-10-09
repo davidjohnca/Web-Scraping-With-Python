c960e56258175044a6245cd8db2ee8a4f713f71f
davidjohnca <112810154+davidjohnca@users.noreply.github.com>
2022-10-09 01:19:50 +0100

    Create advanced_crawler.py

---
+++ advanced_crawler.py
@@ -0,0 +1,25 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    custom_settings = {
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+        'FEED_URI': 'quotes.json',
+        'FEED_FORMAT': 'json'
+    }
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = [
+        'https://quotes.toscrape.com/page/1/'
+    ]
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('span small::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
